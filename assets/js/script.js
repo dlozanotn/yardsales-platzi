0984b59c218f61be7c5ab82acb58b8b3c5dd7f56
dlozanotn <dlozanotn@gmail.com>
2023-02-17 12:50:55 -0600

    pk

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-window.addEventListener("DOMContentLoaded",()=>{
-    let $btnHamb = document.querySelector(".encabezado__hamburguesa a");
-    let $Menu = document.querySelector(".encabezado__menu-responsive");
+window.addEventListener("DOMContentLoaded", () => {
+  let $btnHamb = document.querySelector(".encabezado__hamburguesa a");
+  let $Menu = document.querySelector(".encabezado__menu-responsive");
 
-    $Menu.hidden = true;
-    $btnHamb.addEventListener("click", (e)=>{
-        e.preventDefault();
+  $Menu.hidden = true;
+  $btnHamb.addEventListener("click", (e) => {
+    e.preventDefault();
 
-        $Menu.hidden = !$Menu.hidden
-        $Menu.classList.toggle("menu_active");
-    })
-})
+    $Menu.hidden = !$Menu.hidden;
+    $Menu.classList.toggle("menu_active");
+  });
+});
